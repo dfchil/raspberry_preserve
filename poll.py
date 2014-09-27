4624c56099ce912d06a151887490bd5032d51139
Daniel Fairchild <daniel.fairchild@gmail.com>
2014-09-27 20:06:15 +0200

    better datamodel...

--- poll.py
+++ poll.py
@@ -27,10 +27,14 @@ def main(argv=None):
     
     ddir = "data"
     
+    pwd = os.path.dirname(os.path.realpath(__file__))
+    
+    
     p_result = None
     while p_result == None:
         p_result = poll()
-    with open("%s/%s.data" % (ddir, time.strftime(dfilename_fmt())), "a") as outfile:
+
+    with open("%s/%s/%s.data" % (pwd, ddir, time.strftime(dfilename_fmt())), "a") as outfile:
         outfile.write("%s,%.2f,%.02f\n" % (time.strftime(dformat()), p_result[0], p_result[1]))
     	
 if __name__ == "__main__":
