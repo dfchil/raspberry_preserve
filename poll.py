31a755a2d52174430860a22ef1787ba5365ee56a
Daniel Fairchild <daniel.fairchild@gmail.com>
2014-09-27 19:51:32 +0200

    better datamodel...

--- poll.py
+++ poll.py
@@ -30,9 +30,7 @@ def main(argv=None):
     p_result = None
     while p_result == None:
         p_result = poll()
-    
-    print "%s/%s.dt" % (ddir, time.strftime(dfilename_fmt()))
-    with open("%s/%s.dt" % (ddir, time.strftime(dfilename_fmt())), "a") as outfile:
+    with open("%s/%s.data" % (ddir, time.strftime(dfilename_fmt())), "a") as outfile:
         outfile.write("%s,%.2f,%.02f\n" % (time.strftime(dformat()), p_result[0], p_result[1]))
     	
 if __name__ == "__main__":
