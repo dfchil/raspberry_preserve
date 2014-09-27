770124c9ab98612f8c272fc34b412ebefc65c7d6
Daniel Fairchild <daniel.fairchild@gmail.com>
2014-09-27 18:58:16 +0200

    get handling

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 
+# Import modules for CGI handling 
+import cgi, cgitb 
+
+# Create instance of FieldStorage 
+
 if __name__ == "__main__":
-        print "hello world"
+    form = cgi.FieldStorage() 
+    begin = form.getvalue('begin')
+    end  = form.getvalue('end')
+    
+    print begin, end
