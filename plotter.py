5d97fcec0bb9cea29b05f0ffd6461fc060bb4a05
Daniel Fairchild <daniel.fairchild@gmail.com>
2014-09-28 12:17:54 +0200

    CGI ?

--- plotter.py
+++ plotter.py
@@ -8,14 +8,11 @@ import plot
 # Create instance of FieldStorage 
 
 if __name__ == "__main__":
-#	cgi.test()
 	print "Content-type:text/html\r\n\r\n"
 
 	cgitb.enable()
 	form = cgi.FieldStorage()
 	begin = form.getvalue('begin')
 	end  = form.getvalue('end')
-	print plot.draw_svg(None, None)
-    
-    	print begin, end
+	print plot.draw_svg(None, None)    
     
